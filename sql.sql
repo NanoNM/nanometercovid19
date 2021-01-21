e79a8ba1bcb8c4fefde03353ea48b462214a3603
NanoNM <270884295@qq.com>
2021-01-21 19:33:46 +0800

    更新数据库

--- sql.sql
+++ sql.sql
@@ -14,7 +14,6 @@ create table csse_covid_19_all_reports_daily_update
     Case_Fatality_Ratio double       null
 );
 
--- auto-generated definition
 create table csse_covid_19_all_reports_hestory_daily_update
 (
     id                  int auto_increment
@@ -29,5 +28,73 @@ create table csse_covid_19_all_reports_hestory_daily_update
     Combined_Key        varchar(100) null,
     Incident_Rate       double       null,
     Case_Fatality_Ratio double       null
+);
+
+create table tencent_city_data
+(
+    Province_ID  int          not null,
+    cityName     varchar(100) null,
+    healRate     double       null,
+    wzz          int          null,
+    grade        varchar(100) null,
+    heal         int          null,
+    nowConfirm   int          null,
+    dead         int          null,
+    suspect      int          null,
+    deadRate     int          null,
+    todayConfirm int          null,
+    confirm      int          null
+);
+
+create table tencent_city_his_data
+(
+    Province_ID  int          not null,
+    cityName     varchar(100) null,
+    healRate     double       null,
+    wzz          int          null,
+    grade        varchar(100) null,
+    heal         int          null,
+    nowConfirm   int          null,
+    dead         int          null,
+    suspect      int          null,
+    deadRate     int          null,
+    todayConfirm int          null,
+    confirm      int          null
+);
+
+create table tencent_detailed_china_data
+(
+    id             int auto_increment
+        primary key,
+    province       varchar(100) not null,
+    confirm        int          null,
+    healRate       double       null,
+    wzz            int          null,
+    heal           int          null,
+    nowConfirm     int          null,
+    dead           int          null,
+    suspect        int          null,
+    deadRate       double       null,
+    todayConfirm   int          null,
+    wzz_add        int          null,
+    lastUpdateTime varchar(100) null
+);
+
+create table tencent_detailed_china_his_data
+(
+    id             int auto_increment
+        primary key,
+    province       varchar(100) not null,
+    confirm        int          null,
+    healRate       double       null,
+    wzz            int          null,
+    heal           int          null,
+    nowConfirm     int          null,
+    dead           int          null,
+    suspect        int          null,
+    deadRate       double       null,
+    todayConfirm   int          null,
+    wzz_add        int          null,
+    lastUpdateTime varchar(100) null
 );
 
